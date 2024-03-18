a5b744e0258275fb6f5f26ea851802ecb38f29a4
Atharv Mishra <antilogatharv@gmail.com>
2024-03-18 20:41:31 +0530

    Write functions for setting up server address structure

--- http_tcpServer_linux.cpp
+++ http_tcpServer_linux.cpp
@@ -20,13 +20,21 @@ namespace{
 
 namespace http
 {
-    TcpServer::TcpServer(string ip_address, int port)
-        : m_ip_address(ip_address), m_port(port), m_socket(),
-          m_new_socket(), m_incomingMessage(), m_socketAddress(),
-          m_socketAdress_len(sizeof(m_socketAddress),
-          m_serverMessage(buildResponse());
+    TcpServer::TcpServer(std::string ip_address, int port) : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(),
+                                                             m_incomingMessage(),
+                                                             m_socketAddress(), m_socketAddress_len(sizeof(m_socketAddress)),
+                                                             m_serverMessage(buildResponse())
         {
-            startServer();
+            m_socketAddress.sin_family = AF_INET; //sets the address family to IPv4
+            m_socketAddress.sin_port = htons(m_port); //converts the host byte order to network byte order
+            m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str()); // converts the IP address in string format to binary format and then store it server address structure
+
+            if(startServer() != 0){
+                ostringstream ss;
+                 ss<< "Failed to start a server with PORT: " << ntons(m_socketAddress.sin_port);
+                 log(ss.str());
+            }
+
         }
 
     TcpServer::~TcpServer(){
@@ -39,6 +47,13 @@ namespace http
             exitWithError("Cannot create socket");
             return 1;
         }
+
+        if (bind(m_socket, (sockaddr *)&m_socketAddress, m_socketAddress_len) < 0)
+        {
+            exitWithError("Cannot connect socket to address");
+            return 1;
+        }
+
         return 0;
     }
 
@@ -47,4 +62,11 @@ namespace http
         close(m_new_socket);
         exit(0);
     }
+
+    void TcpServer :: startListen(){
+        if(listen(m_socket, 20) < 0){
+            exitWithError("Socket listen failed");
+        }
+    }
+
 } // namespace http
